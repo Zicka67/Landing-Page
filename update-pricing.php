4c9ad22d87a068060be6cdac2e77a0ca09ff052b
Zicka67 <art.book@hotmail.fr>
2023-02-03 15:43:40 +0100

    Modification dans update-pricing du foreach

--- update-pricing.php
+++ update-pricing.php
@@ -4,24 +4,22 @@ require_once('db-functions.php');
 
 // Récupération des données envoyées à partir du formulaire
 $prices = $_POST['price'];
-$sales = $_POST['sale'];
 $bandwidths = $_POST['bandwidth'];
 $onlinespaces = $_POST['onlinespace'];
 $supports = $_POST['support'];
 $domains = $_POST['domain'];
 $fees = $_POST['fees'];
+$sales = $_POST['sale'];        
 
 // Boucle pour mettre à jour les enregistrements dans la base de données
 foreach ($prices as $id => $price) {
-    $feesValue = isset($fees[$id]) ? $fees[$id] : ''; //verification
-    $price = intval($prices[$id]);
-    $support = intval($supports[$id]);
-    // if (isset($_POST['fees']['1'])) {
-    //     $fees = $_POST['fees']['1'];
-    //   } else {
-    //     $fees = "";
-    //   }
-    modifyProduct($prices[$id], $bandwidths[$id], $onlinespaces[$id], $supports[$id], $domains[$id], $fees, $sales[$id], $id);
+    $bandwidth = $bandwidths[$id];
+    $onlinespace = $onlinespaces[$id];
+    $support = $supports[$id];
+    $domain = $domains[$id];
+    $fee = $fees[$id];
+    $sale = $sales[$id];
+    modifyProduct($price, $bandwidth, $onlinespace, $support, $domain, $fee, $sale, $id);
 }
 
 // Redirection vers la page admin.php
